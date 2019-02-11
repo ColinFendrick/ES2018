8762296c501faa4f0c1de4998326ea7b601ab67b
Colin Fendrick <colin.fendrick@gmail.com>
2019-02-11 10:18:51 -0500

    cleanup

--- iterator.js
+++
@@ -1,7 +0,0 @@
-const arr = [10, 20, 30]
-const iterator = arr[Symbol.iterator]()
-
-console.log(iterator.next()) // → { value: 10, done: false }
-console.log(iterator.next()) // → { value: 20, done: false }
-console.log(iterator.next()) // → { value: 30, done: false }
-console.log(iterator.next()) // → { value: undefined, done: true }
